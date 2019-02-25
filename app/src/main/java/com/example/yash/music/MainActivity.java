98868ccc6f073e669f9e373eb0d12dd9ced20c27
Yash Johri <yashjohri1200@gmail.com>
2019-02-25 13:31:04 +0530

    Songs List

--- app/src/main/java/com/example/yash/music/MainActivity.java
+++ app/src/main/java/com/example/yash/music/MainActivity.java
@@ -1,19 +1,44 @@
 package com.example.yash.music;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION=1;
+    ListView listView;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listview);
+
+        runtimepermission();
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,8 +54,71 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(MainActivity.this,LoginActivity.class));
                 finish();
-                return true;
+                break;
+            case R.id.about:
+                startActivity(new Intent(MainActivity.this,AboutActivity.class));
+
         }
         return false;
+    }
+
+    public void runtimepermission()
+    {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                        displaysongs();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public ArrayList<File> findsong(File file)
+    {
+        ArrayList<File> arrayList=new ArrayList<>();
+
+        File[] files=file.listFiles();
+        for(File singlefiles:files)
+        {
+            if(singlefiles.isDirectory()&&!singlefiles.isHidden())
+            {
+                arrayList.addAll(findsong(singlefiles));
+            }
+            else
+            {
+                if(singlefiles.getName().endsWith(".mp3")||singlefiles.getName().endsWith(".wav"))
+                {
+                    arrayList.add(singlefiles);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    void displaysongs()
+    {
+        final ArrayList<File> mysongs=findsong(Environment.getExternalStorageDirectory());
+        items=new String[mysongs.size()];
+
+        for(int i=0;i<mysongs.size();i++)
+        {
+            items[i]=mysongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
+        }
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adapter);
     }
 }
